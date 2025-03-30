d3d4ab691c43db0c3cdb00eaba0f094e15f2f8ab
Zion2-6 <syzzang326@gmail.com>
2025-03-31 00:46:08 +0900

    .env(gitignore)

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 import os
+from dotenv import load_dotenv
 
-OPENAI_API_KEY = "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"  # 🔹 OpenAI API 키 입력 필요
-FINE_TUNED_MODEL_ID = "ft:gpt-3.5-turbo-0125:personal::B0LjeTdb"
-SECRET_KEY = "EvmYUavrGG7FBnjXFrAVyv2pSpkwPE/jO2j/ecC4vz4="
-GOOGLE_APPLICATION_CREDENTIALS = "helical-history-450910-v7-9d92c447277a.json"
+# 환경 변수 로드
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+FINE_TUNED_MODEL_ID = os.getenv("FINE_TUNED_MODEL_ID")
+SECRET_KEY = os.getenv("SECRET_KEY")
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST", "127.0.0.1"),
-    "port": os.getenv("DB_PORT", "5432"),
-    "database": os.getenv("DB_NAME", "test"),
-    "user": os.getenv("DB_USER", "postgres"),
-    "password": os.getenv("DB_PASSWORD", "ummong1330")
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "database": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
 }
